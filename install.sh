d3706814142f052476914a5051b86009668efe39
Hamed Ap <109583222+hamedap@users.noreply.github.com>
2022-11-26 11:20:44 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,3 +37,17 @@ cp ca-cert.pem server-key.pem server-cert.pem /etc/ocserv/ssl/
 cd /etc/ocserv/
 wget -N https://raw.githubusercontent.com/hamedap/ocserv-centos7/main/ocserv.conf
 
+yum install iptables-services -y > /dev/null &
+wait
+
+iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -s 192.168.1.0/24 -j ACCEPT
+iptables -A FORWARD -j REJECT
+iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -o venet0 -j MASQUERADE
+
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf &
+
+
+systemctl start ocserv
+systemctl enable ocserv
+echo "Finished ! :) Have Fun "
